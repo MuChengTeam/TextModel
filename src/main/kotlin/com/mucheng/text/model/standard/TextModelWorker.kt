afdf936833e831a0dd1931a65a2bb2c54a81e686
CaiMuCheng <2632588830@qq.com>
2022-08-24 20:46:13 +0800

    update comment

--- src/main/kotlin/com/mucheng/text/model/standard/TextModelWorker.kt
+++ src/main/kotlin/com/mucheng/text/model/standard/TextModelWorker.kt
@@ -3,9 +3,19 @@ package com.mucheng.text.model.standard
 import java.io.Reader
 import java.io.Writer
 
+/**
+ * 此类封装了 TextModel 的常用功能
+ * */
 @Suppress("LocalVariableName")
 object TextModelWorker {
 
+    /**
+     * 将 TextModel 的内容写到 Writer 中
+     *
+     * @param textModel 文本模型
+     * @param writer 目标的 Writer 对象
+     * @param onSaveProgress 当每次行存储时都会调用这个 block, 返回 false 即可中断操作
+     * */
     fun save(textModel: TextModel, writer: Writer, onSaveProgress: () -> Boolean = { true }) {
         val bufferedWriter = writer.buffered()
         writer.use {
@@ -29,6 +39,13 @@ object TextModelWorker {
         }
     }
 
+    /**
+     * 从 reader 读取内容到 textModel 中
+     *
+     * @param textModel 文本模型
+     * @param reader 目标的 Reader 对象
+     * @param onReadProgress 当每次行读取时都会调用这个 block, 返回 false 即可中断操作
+     * */
     fun read(textModel: TextModel, reader: Reader, onReadProgress: () -> Boolean = { true }) {
         val LF = CharTable.LF.toString()
         val bufferedReader = reader.buffered()
@@ -54,6 +71,13 @@ object TextModelWorker {
         }
     }
 
+    /**
+     * 拷贝一个新的 TextModel 对象
+     *
+     * @param textModel 文本模型
+     * @param onCopyProgress 当每次行拷贝时都会调用这个 block, 返回 false 即可中断操作
+     * @return 拷贝后的 TextModel 对象
+     * */
     fun copy(textModel: TextModel, onCopyProgress: () -> Boolean = { true }): TextModel {
         val copiedTextModel = TextModel(textModel.lastColumn)
         val iterator = textModel.textRowIterator()
